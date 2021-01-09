1b9854c0f140217ed12f0481ba69f3f3caa02b83
hydazz <53986978+hydazz@users.noreply.github.com>
2021-01-09 22:15:25 +1100

    setup versioning

---
+++ update_version_info.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+OVERLAY_VERSION=$(cat package_versions.txt | grep -E "s6-overlay.*?-" | sed -n 1p | cut -c 12- | sed -E 's/-r.*//g')
+
+OLD_OVERLAY_VERSION=$(cat version_info.json | jq -r .overlay_version)
+OLD_PLEX_VERSION=$(cat version_info.json | jq -r .plex_version)
+
+
+sed -i \
+	-e "s/${OLD_OVERLAY_VERSION}/${OVERLAY_VERSION}/g" \
+	-e "s/${OLD_PLEX_VERSION}/${PLEX_VERSION}/g" \
+	README.md
+
+NEW_VERSION_INFO="overlay_version|plex_version
+${OVERLAY_VERSION}|${PLEX_VERSION}
+
+jq -Rn '
+( input  | split("|") ) as $keys |
+( inputs | split("|") ) as $vals |
+[[$keys, $vals] | transpose[] | {key:.[0],value:.[1]}] | from_entries
+' <<<"$NEW_VERSION_INFO" >version_info.json
